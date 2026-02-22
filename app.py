aafe9b49c3dcdef7a67eb7593bd05249195ab138
romainmonnetetudes-beep <romainmonnetetudes@gmail.com>
2026-02-22 12:21:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def resultats():
 if __name__ == "__main__":
     init_db()
     import os
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
